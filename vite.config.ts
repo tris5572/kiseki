18ea19dff18fdd4cdf5b684862e773cb24e714fb
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-09-24 05:02:55 +0900

    chore: GitHub Pages へのデプロイ設定

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  base: "/kiseki/",
   optimizeDeps: {
     exclude: [
       // ローカル起動時、MapLibre が自動生成する Worker URL が開発サーバー上で正しく動作しないため除外
