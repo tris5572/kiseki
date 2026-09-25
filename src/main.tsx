c86305f7d6ab214debdcc05054a47bc9f45d8e17
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-09-26 06:37:09 +0900

    chore: GitHub Pagesでページを表示可能にする

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { setWorkerUrl } from "maplibre-gl";
+import maplibreglWorkerUrl from "maplibre-gl/dist/maplibre-gl-worker.mjs?url";
 import "./index.css";
 import App from "./App.tsx";
+
+/**
+ * GitHub Pages 環境でも Worker を確実に読み込むため、URL を明示設定する
+ */
+setWorkerUrl(maplibreglWorkerUrl);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
